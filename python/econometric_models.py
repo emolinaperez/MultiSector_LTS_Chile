419fac52f897f3ef858fb45c663561d152eafa19
cabenavif <cabenavi@centroenergia.cl>
2020-11-27 01:37:48 -0300

    Add files via upload
    modification of some functions
    addtional functions are added

--- python/econometric_models.py
+++ python/econometric_models.py
@@ -13,16 +13,15 @@ import numpy as np
 
 # @jit(nopython=True)
 def model_other_industries_new(year, gdp):
-
 	# econometric coeficients
 	a = 0.2579
 	b = 0.4540
 	c = 2.5886
-	dem_2017= 33099.16 #[Tcal]
-	
+	dem_2017 = 33099.16  # [Tcal]
+
 	# Creacion de la variable a contener la demanda energetica
 	demand_energy = np.ones(len(year))
-	
+
 	for i in range(len(year)):
 		if year[i] == 2015:
 			demand_energy[i] = dem_2017
@@ -31,8 +30,8 @@ def model_other_industries_new(year, gdp):
 		elif year[i] == 2017:
 			demand_energy[i] = dem_2017
 		else:
-			demand_energy[i]=np.exp(a*np.log(gdp[i]) + b*np.log(demand_energy[i-1]) + c)
-			
+			demand_energy[i] = np.exp(a * np.log(gdp[i]) + b * np.log(demand_energy[i - 1]) + c)
+
 	return demand_energy
 
 
@@ -56,9 +55,31 @@ def model_other_industries(year, gdp_growth, elasticity):
 		elif year[i] == 2017:
 			demand_energy[i] = dem_2017
 		else:
-			demand_energy[i] = demand_energy[i - 1]*(1+gdp_growth[i]*elasticity[i])
+			demand_energy[i] = demand_energy[i - 1] * (1 + gdp_growth[i] * elasticity[i])
 
 	return demand_energy
+
+
+def model_commercial(year, gdp_growth, elasticity):
+	dem_2015 = 13819.9  # [Tcal]
+	dem_2016 = 15599.5  # [Tcal]
+	dem_2017 = 15875.2  # [Tcal]
+
+	# Creacion de la variable a contener la demanda energetica
+	demand_energy = np.ones(len(year))
+
+	for i in range(len(year)):
+		if year[i] == 2015:
+			demand_energy[i] = dem_2015
+		elif year[i] == 2016:
+			demand_energy[i] = dem_2016
+		elif year[i] == 2017:
+			demand_energy[i] = dem_2017
+		else:
+			demand_energy[i] = demand_energy[i - 1] * (1 + gdp_growth[i] * elasticity[i])
+
+	return demand_energy
+
 
 def model_transport_gasoline_demand(year, gdp):
 	# econometric coeficients
@@ -95,7 +116,7 @@ def model_transport_pkm_aviation(year, gdp):
 	c = -1.0978
 
 	pkm_2015 = 10862059  # [pkm]
-	pkm_2016 = 12200515 # [pkm]
+	pkm_2016 = 12200515  # [pkm]
 	pkm_2017 = 13134871  # [pkm]
 
 	# Creacion de la variable a contener la demanda energetica
@@ -113,10 +134,10 @@ def model_transport_pkm_aviation(year, gdp):
 
 	return pkm_aviation
 
-def model_transport_pkm_saturacion(year,transport_demand_aviation_kerosene,transport_saturation_aviation):
 
+def model_transport_pkm_saturacion(year, transport_demand_aviation_kerosene, transport_saturation_aviation):
 	for i in range(len(year)):
 		if transport_demand_aviation_kerosene[i] > transport_saturation_aviation[i]:
-			transport_demand_aviation_kerosene[i] = transport_demand_aviation_kerosene[i-1]*1.001
+			transport_demand_aviation_kerosene[i] = transport_demand_aviation_kerosene[i - 1] * 1.001
 
 	return transport_demand_aviation_kerosene
